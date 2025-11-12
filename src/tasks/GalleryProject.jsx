f9f1b2d2b7cd8576703400a2cb51cbf12bb15190
Vatsaly76 <shuklavatsaly@gmail.com>
2025-11-13 01:32:33 +0530

    fix: improve loading state and clean up pagination button logic in GalleryProject

--- src/tasks/GalleryProject.jsx
+++ src/tasks/GalleryProject.jsx
@@ -11,17 +11,17 @@ const GalleryProject = () => {
         setUserdata(response.data)
         console.log(response.data);
     }
-    
+
     useEffect(() => {
         document.title = "Gallery Project | Axios"
         getData()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [page])
 
-    let printUserData = null;
+    let printUserData = <h3 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl font-semibold">Loading...</h3>;
 
     if (Userdata.length > 0) {
-        printUserData = Userdata.map((user,idx) => {
+        printUserData = Userdata.map((user, idx) => {
             return (
                 <div key={idx} className="p-4 m-4 rounded-2xl bg-white shadow-lg hover:scale-105 transition-transform duration-700 hover:bg-gray-500">
                     <img src={user.download_url} alt={user.author} className="w-full h-64 object-cover mb-2" />
@@ -37,31 +37,33 @@ const GalleryProject = () => {
     return (
         <div className="p-8 bg-gray-200 min-h-screen">
             <h1 className="text-3xl font-bold mb-4">Gallery Project using Axios</h1>
-            
+
             {/* Pagination Buttons */}
-            <div className="flex justify-center mt-2 text-xl space-x-4">
-                <button 
-                onClick={() => {
-                    if(page > 1){
-                        setPage(page - 1);
-                    }
-                }}
-                className="p-3 bg-gray-500 font-semibold text-white rounded-lg cursor-pointer hover:bg-gray-400 hover:scale-105 transition-transform duration-300">
+            <div className="flex justify-center mt-2 text-md space-x-4">
+                <button
+                    onClick={() => {
+                        if (page > 1) {
+                            setPage(page - 1);
+                        }
+                    }}
+                    className="p-3 bg-gray-500 font-semibold text-white rounded-lg cursor-pointer hover:bg-gray-400 hover:scale-105 transition-transform duration-300
+                disabled:opacity-30 disabled:cursor-not-allowed disabled:scale-100 disabled:bg-gray-500" disabled={page === 1}>
                     <i class="ri-arrow-left-line"></i>
                     Prev</button>
+                <div className="p-3 bg-amber-400 rounded-lg font-semibold text-gray-800">Page {page}</div>
                 <button
-                onClick={() => {
-                    setPage(page + 1);
-                }}
-                 className="p-3 bg-gray-500 font-semibold text-white rounded-lg cursor-pointer hover:bg-gray-400 hover:scale-105 transition-transform duration-300">Next
-                 <i class="ri-arrow-right-line"></i></button>
+                    onClick={() => {
+                        setPage(page + 1);
+                    }}
+                    className="p-3 bg-gray-500 font-semibold text-white rounded-lg cursor-pointer hover:bg-gray-400 hover:scale-105 transition-transform duration-300">Next
+                    <i class="ri-arrow-right-line"></i></button>
             </div>
 
 
             <div className="mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {printUserData}
             </div>
-            
+
         </div>
     )
 }
